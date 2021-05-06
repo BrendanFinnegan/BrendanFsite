047e3f5a9f33d01b01eba7ad6c1d81e0df1c0a09
BrendanFinnegan <bf289907@gmail.com>
2021-05-06 11:34:27 -0400

    barvis story

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.addEventListener('click',(event)=>{console.log(event)})
+document.addEventListener('click',(test)=>{console.log(test)})
+
+
+let changeHeader = () => {
+    if (document.querySelector('h1').style.color === 'blue') {document.querySelector('h1').style.color = 'green'} else {
+        document.querySelector('h1').style.color = 'blue'}
+    }
+document.addEventListener('keydown', (e) => {if (e.key === 'ArrowRight') 
+{changeHeader()}})
+console.log('heyyy')
+console.log(document.getElementsByClassName('poo'))
